a944acd1d344dd6c54c7c5712dfa6574f932ab39
Matthew Smith <matthew@apptreesoftware.com>
2019-05-01 13:41:42 -0700

    update fields

--- source.go
+++ source.go
@@ -8,7 +8,7 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-const sourceKey = "logging.googleapis.com/sourceLocation"
+const sourceKey = "sourceLocation"
 
 // SourceLocation adds the correct Stackdriver "SourceLocation" field.
 //
